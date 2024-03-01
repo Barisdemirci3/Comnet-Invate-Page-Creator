99ecead95c04b9c4dc6313c62056100cd5b37b4d
Barisdemirci3 <baris19052003@gmail.com>
2024-03-02 01:48:09 +0300

    Sweetalert2 added.

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.min.css">
     <title>Comnet İnvite Page Generator</title>
 </head>
 
@@ -25,13 +26,19 @@
     </div>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.5/dist/sweetalert2.all.min.js"></script>
 <script>
     addEventListener('submit', function(e){
         e.preventDefault();
         var invitecode = document.querySelector('input[name="invite_code"]').value;
         if(invitecode == ""){
-            alert("Davet kodu boş olamaz");
+            Swal.fire({
+                icon: 'error',
+                title: 'Hata',
+                text: 'Davet kodu boş olamaz!',
+                confirmButtonText: 'Tamam',
+                confirmButtonColor: 'green'
+            });
         }else{
             window.location.href = "page.php?invite_code="+invitecode;
         }
